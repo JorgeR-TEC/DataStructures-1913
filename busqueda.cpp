ff1bb780af6da7f4dda3b0bdba81cc47a987bb0c
jorge14rodriguez <jorge@L03040561L01.svcs.itesm.mx>
2019-09-30 11:25:11 -0600

    agregada busqueda ordenada 2

--- busqueda.cpp
+++ busqueda.cpp
@@ -27,6 +27,32 @@ int busquedaOrdenada1(int *arreglo, int size, int buscado){
 
 }
 
+int ordenada2Aux(int *arreglo, int lo, int hi, int buscado){
+	for(int i=lo; i<=hi; i++){
+		if(arreglo[i]==buscado){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int busquedaOrdenada2(int *arreglo, int size, int buscado){
+	int paso=50;
+	for(int i=0; i<size; i+=paso){
+		if((i+paso)<size){//caso normal
+			if(arreglo[i+paso]>buscado){
+				return ordenada2Aux(arreglo, i, i+paso, buscado);
+			}
+		}else{//ultimo tramo
+			if(arreglo[size-1]>buscado){
+				return ordenada2Aux(arreglo, i, size-1, buscado);
+			}
+		}
+	}
+	return -1;
+
+}
+
 int main(){
 	int size=10000;
 	int a[size];
@@ -39,16 +65,24 @@ int main(){
 	}
 	
 	auto inicio=high_resolution_clock::now();
-	int posicion=busquedaSecuencial(a, size, 100);
+	int posicion=busquedaSecuencial(a, size, 1000);
 	auto fin=high_resolution_clock::now();
 	cout<<"posicion: "<<posicion<<endl;
 	auto duracion=fin-inicio;
 	cout<<"tiempo: "<<duration_cast<microseconds>(duracion).count()<<endl;
 	//
 	inicio=high_resolution_clock::now();
-	posicion=busquedaOrdenada1(a, size, 100);
+	posicion=busquedaOrdenada1(a, size, 1000);
 	fin=high_resolution_clock::now();
 	cout<<"posicion Ordenada: "<<posicion<<endl;
 	duracion=fin-inicio;
 	cout<<"tiempo Ordenada1: "<<duration_cast<microseconds>(duracion).count()<<endl;
+	//ordenada2
+	inicio=high_resolution_clock::now();
+	posicion=busquedaOrdenada2(a, size, 1000);
+	fin=high_resolution_clock::now();
+	cout<<"posicion Ordenada2: "<<posicion<<endl;
+	duracion=fin-inicio;
+	cout<<"tiempo Ordenada2: "<<duration_cast<microseconds>(duracion).count()<<endl;
+	
 }
